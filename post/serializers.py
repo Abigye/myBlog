ce7db4129184867fec3ed8878e3a761172a1c3f5
Abigye <49094633+Abigye@users.noreply.github.com>
2020-06-25 21:18:58 +0100

    Delete serializers.py

--- post/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Article
-
-
-class ArticleSerializers(serializers.ModelSerializer):
-
-    class Meta:
-        model = Article
-        fields = ['id' , 'title', 'preview', 'content']
