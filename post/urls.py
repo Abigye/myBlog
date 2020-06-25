81a8dc87c314303cb9f8e807677e2b5ac1b73c1a
Abigye <49094633+Abigye@users.noreply.github.com>
2020-06-25 21:22:38 +0100

    Update urls.py

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from post.views import all_post, post_detail
-# , all_post_api, article_list
+
 
 urlpatterns = [
     path('', all_post, name="all_post"),
-    path('post/<int:id>/', post_detail, name= "pots_detail"),
-#     path('postapi/', all_post_api),
-#     path('api/posts/', article_list),
+    path('post/<int:id>/', post_detail, name= "post_detail"),
+
 ]
